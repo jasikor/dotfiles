f62d0f8349e6c96a4a2916ef594af9b06ef919e1
ja <jasikor@gmail.com>
2026-01-23 13:50:18 +0100

    Initial commit

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = false
+vim.opt.number = false
+vim.opt.undofile = false
+vim.opt.guicursor = "n-v-c:block-blinkon100-blinkoff100,i:ver25-blinkon200"
---
+++ dot_config/nvim/lua/plugins/symlink_theme.lua
@@ -0,0 +1,1 @@
+/home/ja/.config/omarchy/current/theme/neovim.lua
