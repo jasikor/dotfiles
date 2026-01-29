0a201409e038fc69185f45696d2ceef25fa4c14f
ja <jasikor@gmail.com>
2026-01-29 11:55:58 +0100

    nvim macro on F4 and F5

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here;
 vim.keymap.set('n', '<Del>', '<Nop>', { noremap = true, silent = true })
+-- Start/Stop recording into register q
+-- Toggle recording in register q
+
+vim.keymap.set('n', '<F4>', function()
+  if vim.fn.reg_recording() == '' then
+    return 'qq'
+  else
+    return 'q'
+  end
+end, { expr = true })
+
+-- Playback register q
+vim.keymap.set('n', '<F5>', '@q')
