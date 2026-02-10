efb45ac3dfda8ce590ee5b3c717273178646ad03
ja <jasikor@gmail.com>
2026-02-10 08:46:31 +0100

    colors.lua removed

--- dot_config/nvim/after/plugin/colors.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1a1a1a" })
-vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#1a1a1a" })
-vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#333333" })
