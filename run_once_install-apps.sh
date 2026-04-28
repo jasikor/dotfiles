536a4262491cd2921443c13c9dfeafa384985922
ja <jasikor@gmail.com>
2026-04-28 11:13:05 +0200

    installer: add syncthing

--- run_once_install-apps.sh
+++ run_once_install-apps.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 omarchy-install-terminal kitty
-sudo pacman -S --needed --noconfirm ttf-iosevka-nerd bitwarden rust-analyzer yazi
+sudo pacman -S --needed --noconfirm ttf-iosevka-nerd bitwarden rust-analyzer yazi syncthing
 cargo install --locked bacon
