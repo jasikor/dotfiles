31e338ef07f0c7a700b1edfa93ddd664cc0657e9
ja <jasikor@gmail.com>
2026-04-29 08:09:21 +0200

    cursor in inactive window

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -10,3 +10,33 @@ vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
   pattern = "*",
   command = "silent! wa",
 })
+
+-- Fake cursor in inactive windows
+local ns = vim.api.nvim_create_namespace("fake_cursor")
+local fake_cursors = {}
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_get_current_buf()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    if fake_cursors[win] then
+      pcall(vim.api.nvim_buf_del_extmark, buf, ns, fake_cursors[win])
+    end
+    fake_cursors[win] = vim.api.nvim_buf_set_extmark(buf, ns, cursor[1] - 1, cursor[2], {
+      virt_text = { { "█", "FakeCursorInactive" } },
+      virt_text_pos = "overlay",
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    if fake_cursors[win] then
+      local buf = vim.api.nvim_get_current_buf()
+      pcall(vim.api.nvim_buf_del_extmark, buf, ns, fake_cursors[win])
+      fake_cursors[win] = nil
+    end
+  end,
+})
