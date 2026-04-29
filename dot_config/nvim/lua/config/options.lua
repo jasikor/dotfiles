3f93af3d076bacb396e602a69da521f15df9bbb2
ja <jasikor@gmail.com>
2026-04-29 07:31:04 +0200

    bright cursor

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 vim.opt.relativenumber = false
 vim.opt.number = false
 vim.opt.undofile = false
-vim.opt.guicursor = "n-v-c:block-blinkon100-blinkoff100,i:ver25-blinkon200"
+vim.opt.guicursor = "n-v-c:block-Cursor-blinkon100-blinkoff100,i:ver25-Cursor-blinkon200"
 vim.opt.list = false
 vim.g.snacks_animate = false
 vim.opt.swapfile = false
