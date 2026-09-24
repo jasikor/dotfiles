8ad97c72db937b5020d27d44233a0547c02d6f02
ja <jasikor@gmail.com>
2026-09-24 09:08:02 +0200

    Super-L switches layouts

--- dot_config/hypr/bindings.lua
+++ dot_config/hypr/bindings.lua
@@ -2,8 +2,16 @@
 o.bind("SUPER + m", "Add master window", hl.dsp.layout("addmaster"))
 o.bind("SUPER + SHIFT + m", "Remove master window", hl.dsp.layout("removemaster"))
 
+-- Toggle active workspace between master and dwindle layouts
+hl.unbind("SUPER + L") -- was: Toggle workspace layout (omarchy default)
+local function toggle_layout()
+  local ws = hl.get_active_workspace()
+  local next_layout = (ws.tiled_layout == "master") and "dwindle" or "master"
+  hl.workspace_rule({ workspace = ws.name, layout = next_layout })
+end
+o.bind("SUPER + L", "Toggle layout master/dwindle", toggle_layout)
+
 -- Unbind defaults
-hl.unbind("SUPER + L")
 hl.unbind("SUPER + SHIFT + SPACE")
 hl.unbind("SUPER + P")
 hl.unbind("SUPER + G")
